d6a03e583367156e1a76000bf0c6969d6d844e27
NysoS <nwanysos79@gmail.com>
2023-01-04 21:51:28 +0100

    #1 adding gameManager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager GameManagerInstance;
+
+    [SerializeField]
+    private GameObject playerController = null;
+
+    private void Awake()
+    {
+        if(!GameManagerInstance && GameManagerInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        GameManagerInstance = this;
+
+        SpawnPlayerController();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlayerController()
+    {
+        if (!playerController)
+        {
+            Debug.LogError("no playerController found");
+            return;
+        }
+        Instantiate(playerController, transform.position, Quaternion.identity);
+    }
+
+    public GameObject GetPlayerController()
+    {
+        return playerController;
+    }
+}
